7e18d936b4b3d59f46a69881f5b622e5945df380
NJ <nicolas.jeuniaux.pro@hotmail.com>
2020-10-17 17:59:00 +0200

    First test

--- application/main.js
+++ application/main.js
@@ -3,6 +3,9 @@ const path = require('path');
 const bodyparser = require('body-parser');
 const form_control = require('./controller/form_control');
 const app = express();
+const https = require('https');
+const http = require('http');
+const fs = require('fs');
 const port = 8060;
 
 app.set('view engine', 'ejs');
@@ -22,7 +25,15 @@ app.post('/jsonreception', bodyparser.json(), (req, res) => {
     }
 });
 
+var credentials = {
+    key: fs.readFileSync('ssl/Dipicar_ssl.key', 'utf-8'),
+    cert: fs.readFileSync('ssl/Dipicar_ssl.crt', 'utf-8')
+};
 
-app.listen(port, () => {
-    console.log(`The app is running and listening to the port ${port}.`);
+http.createServer(app).listen(port+1, () => {
+    console.log(`The http server is running and listening to the port ${port+1}.`);
+});
+
+https.createServer(credentials, app).listen(port, () => {
+    console.log(`The https server is running and listening to the port ${port}.`);
 });
